12cc53cb679087101e1b026fc9dc9e6410975548
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-04-13 15:10:06 -0400

    Update docs

--- docs/search/functions_5.js
+++ docs/search/functions_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['lidarview_0',['LidarView',['../class_lidar_view.html#abfada6e9607893414f149398aa46fc13',1,'LidarView']]]
+  ['lidarview_0',['LidarView',['../class_lidar_view.html#abfada6e9607893414f149398aa46fc13',1,'LidarView::LidarView()'],['../class_lidar_view.html#a9fa3d0403a0953aef96e2e604df8bbbd',1,'LidarView::LidarView(std::vector&lt; icp::Point &gt; source, std::vector&lt; icp::Point &gt; destination)']]]
 ];
