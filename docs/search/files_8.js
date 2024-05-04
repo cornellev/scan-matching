3c3df33de321e474f5bd9f4cadb73716976c4ca9
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-05-04 19:04:11 -0400

    Update docs

--- docs/search/files_8.js
+++ docs/search/files_8.js
@@ -1,8 +1,6 @@
 var searchData=
 [
   ['vanilla_2ecpp_0',['vanilla.cpp',['../vanilla_8cpp.html',1,'']]],
-  ['vanilla_2ed_1',['vanilla.d',['../vanilla_8d.html',1,'']]],
-  ['view_5fconfig_2ecpp_2',['view_config.cpp',['../view__config_8cpp.html',1,'']]],
-  ['view_5fconfig_2ed_3',['view_config.d',['../view__config_8d.html',1,'']]],
-  ['view_5fconfig_2eh_4',['view_config.h',['../view__config_8h.html',1,'']]]
+  ['view_5fconfig_2ecpp_1',['view_config.cpp',['../view__config_8cpp.html',1,'']]],
+  ['view_5fconfig_2eh_2',['view_config.h',['../view__config_8h.html',1,'']]]
 ];
